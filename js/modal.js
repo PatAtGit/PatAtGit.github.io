eb6d5dd2f4213b7c3698ba254b4606fadf66c79c
PatAtGit <35389689+PatAtGit@users.noreply.github.com>
2020-10-18 00:30:26 -0400

    modal imgs and general site format

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,20 @@
-    // Get the modal
-    var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal");
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+const imgs = document.querySelectorAll(".img-bg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+imgs.forEach(img =>
+    {
+        img.onclick = function(){
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+          }
+    })
 
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-    modal.style.display = "block";
-    }
+var span = document.getElementsByClassName("modal")[0];
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-    modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-    }
+span.onclick = function() { 
+  modal.style.display = "none";
+}
